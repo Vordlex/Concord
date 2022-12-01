4f1133402eeac7423eee3daa915969e47a9fb630
Pablo h.c <pablo.dev.carvalho@gmail.com>
2022-11-30 22:03:57 -0300

    adding types in embed_create type

---
+++ types/embed_create.d.ts
@@ -0,0 +1,48 @@
+export type embed_create = {
+    title?: string;
+    type?: "rich" | "image" | "video" | "gifv" | "article" | "link";
+    description?: string;
+    url?: string;
+    timestamp?: Date;
+    color?: number | string;
+    footer?: {
+        text?: string;
+        icon_url?: string;
+        proxy_icon_url?: string;
+    };
+    image?: {
+        url?: string;
+        proxy_url?: string;
+        height?: string;
+        width?: string;
+    };
+    thumbnail?: {
+        url?: string;
+        proxy_url?: string;
+        height?: string;
+        width?: string;
+    };
+    video?: {
+        url?: string;
+        proxy_url?: string;
+        height?: string;
+        width?: string;
+    };
+    provider?: {
+        name?: string;
+        url?: string;
+    };
+    author?: {
+        name?: string;
+        url?: string;
+        icon_url?: string;
+        proxy_icon_url?: string;
+    };
+    fields?: Field[]
+}
+
+export type Field = {
+    name: string;
+    value: string;
+    inline?: boolean
+}
