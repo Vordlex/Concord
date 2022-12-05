57cebde337e1281641cf254dafd90dc03ada654e
Pablo h.c <pablo.dev.carvalho@gmail.com>
2022-12-05 13:58:03 -0300

    adding svg voice channel and fix channel type

--- src/Pages/Components/LeftTab/channel/channel.tsx
+++ src/Pages/Components/LeftTab/channel/channel.tsx
@@ -1,17 +1,100 @@
-import fetchMessages from "../../../../functions/fetchMessages"
-import styles from "./channel.module.css"
+import fetchMessages from "../../../../functions/fetchMessages";
+import styles from "./channel.module.css";
 
 const Channel = ({
   name,
   type,
   channelId,
 }: {
-  name: string
-  type: 0 | 2 | 4 | 5
-  channelId: string
+  name: string;
+  type: 0 | 2 | 4 | 5;
+  channelId: string;
 }) => {
   return (
     <div className={styles.main}>
+      {type === 2 ? (
+        /*Websocket aq*/ <VoiceChannel
+          channelId={channelId}
+          channelName={name}
+          key={channelId}
+        />
+      ) : (
+        <TypeChannel
+          key={channelId}
+          channelName={name}
+          channelId={channelId}
+          channelType={type}
+        />
+      )}
+    </div>
+  );
+};
+
+function VoiceChannel({
+  channelName,
+  channelId,
+}: {
+  channelName: string;
+  channelId: string;
+}) {
+  return (
+    <>
+      <div>
+        <svg
+          className="icon-2W8DHg"
+          aria-hidden="true"
+          role="img"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M11.383 3.07904C11.009 2.92504 10.579 3.01004 10.293 3.29604L6 8.00204H3C2.45 8.00204 2 8.45304 2 9.00204V15.002C2 15.552 2.45 16.002 3 16.002H6L10.293 20.71C10.579 20.996 11.009 21.082 11.383 20.927C11.757 20.772 12 20.407 12 20.002V4.00204C12 3.59904 11.757 3.23204 11.383 3.07904ZM14 5.00195V7.00195C16.757 7.00195 19 9.24595 19 12.002C19 14.759 16.757 17.002 14 17.002V19.002C17.86 19.002 21 15.863 21 12.002C21 8.14295 17.86 5.00195 14 5.00195ZM14 9.00195C15.654 9.00195 17 10.349 17 12.002C17 13.657 15.654 15.002 14 15.002V13.002C14.551 13.002 15 12.553 15 12.002C15 11.451 14.551 11.002 14 11.002V9.00195Z"
+            aria-hidden="true"
+          ></path>
+        </svg>
+      </div>
+      <div key={channelId}>{channelName}</div>
+    </>
+  );
+}
+
+function TypeChannel({
+  channelName,
+  channelType,
+  channelId,
+}: {
+  channelName: string;
+  channelType: 0 | 2 | 4 | 5;
+  channelId: string;
+}) {
+  if (channelType === 4) {
+    return (
+      <>
+        <div key={channelId}>Category{channelName}</div>
+      </>
+    );
+  }
+  if (channelType === 5) {
+    return (
+      <>
+        <div
+          onClick={() => {
+            fetchMessages(channelId);
+          }}
+          key={channelId}
+        >
+          ANNOUNCEMENT{channelName}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
       <div>
         <svg width="24" height="24" viewBox="0 0 24 24" role="img">
           <path
@@ -24,13 +107,14 @@ const Channel = ({
       <div
         className={styles.channelName}
         onClick={() => {
-          fetchMessages(channelId)
+          fetchMessages(channelId);
         }}
+        key={channelId}
       >
-        {name}
+        {channelName}
       </div>
-    </div>
-  )
+    </>
+  );
 }
 
-export default Channel
+export default Channel;
