cd559a80c969b2db6398c5666c9d8306477efb64
Pablo h.c <pablo.dev.carvalho@gmail.com>
2022-12-06 18:38:57 -0300

    create inputTest component + react-hook-form package

--- src/Pages/Components/MiddleTab/MiddleTab.tsx
+++ src/Pages/Components/MiddleTab/MiddleTab.tsx
@@ -2,14 +2,14 @@ import { connect } from "react-redux"
 import { messages } from "../../../../types/messages_type"
 import styles from "./MiddleTab.module.css"
 import Message from "./Message/message"
-import InputText from "./Input/InputText"
+import InputText from "./Input/inputTest";
 
 const MiddleTab = ({ messages_redux }: { messages_redux: messages[] }) => {
   return (
     <div className={styles.main}>
       <div className={styles.submain}>
         {messages_redux.reverse().map((message) => {
-          return <Message message={message} key={message.id} />
+          return <Message message={message} key={message.id} />;
         })}
       </div>
       {messages_redux.length === 0 ? (
@@ -18,8 +18,8 @@ const MiddleTab = ({ messages_redux }: { messages_redux: messages[] }) => {
         <InputText channel={messages_redux[0].channel_id} />
       )}
     </div>
-  )
-}
+  );
+};
 const mapStateToProps = ({
   messages_redux,
 }: {
