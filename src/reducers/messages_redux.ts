e523200ec02a816ddb442dad8646205c3f400746
Pablo h.c <pablo.dev.carvalho@gmail.com>
2022-12-12 05:10:50 -0300

    fix(message_redux): possible solution in 'MESSAGE_CREATE'

--- src/reducers/messages_redux.ts
+++ src/reducers/messages_redux.ts
@@ -2,20 +2,40 @@ import { messages } from "../../types/messages_type"
 
 const initialState: messages[] = []
 
+// export const messages_redux = (
+//   state: messages[] = initialState,
+//   action: { type: string; data: messages[] }
+// ): messages[] => {
+//   if (action.type === "CHANNEL_FETCH") {
+//     return action.data
+//   }
+//   if (action.type === "MESSAGE_CREATE") {
+//     if (state.length > 0) {
+//       const { d } = action.data as unknown as { d: messages }
+//       if (state[0].channel_id === d.channel_id) {
+//         return state
+//       }
+//     }
+//   }
+//   return state
+// }
+
+
+// ---> Possivel solução
+
 export const messages_redux = (
   state: messages[] = initialState,
   action: { type: string; data: messages[] }
 ): messages[] => {
   if (action.type === "CHANNEL_FETCH") {
-    return action.data
+    state = action.data
+    return state
   }
   if (action.type === "MESSAGE_CREATE") {
-    if (state.length > 0) {
-      const { d } = action.data as unknown as { d: messages }
-      if (state[0].channel_id === d.channel_id) {
-        return state
-      }
-    }
+    const message = action.data
+    state.push(...message)
+
+    return state
   }
   return state
 }
