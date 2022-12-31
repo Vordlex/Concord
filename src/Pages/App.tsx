6f68b5e3d30ed112ed41143d7a03138bc318094b
Pablo h.c <pablo.dev.carvalho@gmail.com>
2022-12-30 21:07:29 -0300

    add lazy loading and routes in App

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -1,30 +1,39 @@
-import { connect } from "react-redux"
-import { Websocket_Reducers_Type } from "../reducers/websocket_redux"
-import Guilds from "./Components/Guilds/guilds"
-import LeftTab from "./Components/LeftTab/LeftTab"
-import MiddleTab from "./Components/MiddleTab/MiddleTab"
+import { connect } from 'react-redux'
+import { Websocket_Reducers_Type } from '../reducers/websocket_redux'
+import Spinner from './Components/Loading/Spinner'
+import { Suspense, lazy } from 'react'
+
+//Lazy
+const VerifyLogin = lazy(() => import('./auth/verifyLogin'))
+const Guilds = lazy(() => import('./Components/Guilds/guilds'))
+const LeftTab = lazy(() => import('./Components/LeftTab/LeftTab'))
+const MiddleTab = lazy(() => import('./Components/MiddleTab/MiddleTab'))
 
 const App = (...props: any) => {
-  const {
-    websocket_redux,
-  }: {
-    websocket_redux: Websocket_Reducers_Type
-  } = props[0]
-  return (
-    <div>
-      <Guilds />
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        {websocket_redux.LeftTabEnabled === true ? <LeftTab /> : ""}
-        <MiddleTab />
-      </div>
-    </div>
-  )
+    const {
+        websocket_redux,
+    }: {
+        websocket_redux: Websocket_Reducers_Type
+    } = props[0]
+    return (
+        <Suspense fallback={<Spinner />}>
+            <VerifyLogin>
+                <div>
+                    <Guilds />
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        {websocket_redux.LeftTabEnabled === true ? <LeftTab /> : ''}
+                        <MiddleTab />
+                    </div>
+                </div>
+            </VerifyLogin>
+        </Suspense>
+    )
 }
 
 const mapStateToProps = (state: Websocket_Reducers_Type) => {
-  return {
-    ...state,
-  }
+    return {
+        ...state,
+    }
 }
 
 export default connect(mapStateToProps)(App)
